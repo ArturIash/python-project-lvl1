4b98df8fb78fbb949f71e29941a559f04f9f4d18
ArturIash <4ezyyy@gmail.com>
2022-09-22 16:57:06 +0300

    add brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games import even_game
+from brain_games.logic import run_game
+
+
+def main():
+    run_game(even_game)
+
+
+if __name__ == '__main__':
+    main()
