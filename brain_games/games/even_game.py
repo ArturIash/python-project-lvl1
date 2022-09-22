f273c044c7be0e481892fb606943e3a844221dbf
ArturIash <4ezyyy@gmail.com>
2022-09-22 21:31:01 +0300

    add even_game.py

---
+++ brain_games/games/even_game.py
@@ -0,0 +1,10 @@
+import random
+
+
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def get_question_and_answer():
+    random_number = random.randint(1, 50)
+    correct_answer = 'yes' if random_number % 2 == 0 else 'no'
+    return correct_answer, random_number
