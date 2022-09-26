742eb846d5bb101c9e6891cd075a9f390bfbc8a9
ArturIash <4ezyyy@gmail.com>
2022-09-26 17:01:20 +0300

    add calc_game.py

---
+++ brain_games/games/calc_game.py
@@ -0,0 +1,21 @@
+import random
+import operator
+
+
+DESCRIPTION = 'What is the result of the expression?'
+
+
+def get_question_and_answer():
+    """Create randon +, _ or * with 2 numbers and check answer"""
+    actions = {
+        '+': operator.add,
+        '*': operator.mul,
+        '-': operator.sub
+    }
+    first_num = random.randint(1, 14)
+    second_num = random.randint(1, 14)
+    random_action = random.choice(list(actions))
+    math_question = f'{first_num} {random_action} {second_num}'
+    correct_answer = actions[random_action](first_num, second_num)
+
+    return str(correct_answer), math_question
