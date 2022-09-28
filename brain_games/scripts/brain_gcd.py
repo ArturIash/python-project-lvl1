54e0da3cc3fe6c5258d7a2a11e8904a6b94e64f9
ArturIash <4ezyyy@gmail.com>
2022-09-28 18:36:24 +0300

    add brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.games import gcd_game
+from brain_games.logic import run_game
+
+
+def main():
+    run_game(gcd_game)
+
+
+if __name__ == '__main__':
+    main() 
