72154d8a445d1fdc9066489ca0bd595cf1c9dc0c
ArturIash <4ezyyy@gmail.com>
2022-10-03 07:14:26 +0300

    add progression_game.py

---
+++ brain_games/games/progression_game.py
@@ -0,0 +1,20 @@
+import random
+
+DESCRIPTION = 'What number is missing in the progression?'
+
+
+def get_question_and_answer():
+    start_number = random.randint(3, 9)
+    step = random.randint(3, 9)
+    length = 7
+
+    numbers = list(range(start_number, (start_number + length * step), step))
+
+    hidden_index = random.randrange(0, length)
+    correct_answer, numbers[hidden_index] = numbers[hidden_index], '..'
+
+    progression = ' '.join(map(str, numbers))
+
+    math_message = f'{progression}'
+
+    return str(correct_answer), math_message
