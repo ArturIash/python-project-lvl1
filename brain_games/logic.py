8b4ab70d13b065234354d850ad9f63d7b8c07303
ArturIash <4ezyyy@gmail.com>
2022-10-03 12:16:16 +0300

    fixed indent

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -9,7 +9,7 @@ COUNT = 3
 
 def run_game(game):
     print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name?')
+    name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
     print(game.DESCRIPTION)
 
