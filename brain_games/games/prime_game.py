24699fe556f6ebfe54cd49456dc41ef5b946f8ea
ArturIash <4ezyyy@gmail.com>
2022-10-07 15:27:47 +0300

    fix #l14-21

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -12,10 +12,9 @@ def get_question_and_answer():
 
 
 def is_prime(num):
-    prime_number = [2, 3, 5, 7]
-    check_num = [num > 1,
-                 num % 2 != 0,
-                 num % 3 != 0,
-                 num % 5 != 0,
-                 num % 7 != 0]
-    return all(check_num) or num in prime_number
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
